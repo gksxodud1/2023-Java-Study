fb36bddfdd623c634028a5e57e247deff8f5b736
gksxodud1 <133472378+gksxodud1@users.noreply.github.com>
2023-05-22 00:16:16 +0900

    2주차 숙제 문제

---
+++ Week2/hantaeyoung.java
@@ -0,0 +1,20 @@
+package Week2;
+
+/*
+public class hantaeyoung {
+    int minus( int x , int y) {
+        int result = x + y;
+        return result;
+    }
+    String standup() {
+        System.out.println("일어납니다.");
+    }
+    String sitdown() {
+        System.out.println("앉습니다.");
+        
+    }
+    int divide(int x, int y) {
+       int result = (int)x / (int)y;
+       return = result;
+}
+*/

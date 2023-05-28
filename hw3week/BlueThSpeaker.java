6d8db2bd0127d323d0aad8d017d44b55f8d75719
gksxodud1 <133472378+gksxodud1@users.noreply.github.com>
2023-05-28 23:12:33 +0900

    정답 파일
    정답 파일 !!!!

--- hw3week/BlueThSpeaker.java
+++ hw3week/BlueThSpeaker.java
@@ -2,7 +2,7 @@ package hw3week;
 
 public class BlueThSpeaker extends Speaker{
     public BlueThSpeaker(String model, String color) {
-        //컴파일 시 자동 추가 되긴 하지만 이 줄에 들어가야할 부모 생성자를 호출해주는 것을 작성해주세용
+        super();
         this.model = model;
         this.color = color;
         System.out.println("BlueThspeaker 모델 컬러 생성자 실행 ");
--- hw3week/BlueThSpeakerExample.java
+++ hw3week/BlueThSpeakerExample.java
@@ -2,7 +2,7 @@ package hw3week;
 
 public class BlueThSpeakerExample {
     public static void main(String[] args) {
-        //BlueThspeaker 객체를  mySpeaker 로 생성해주세요 speaker로 부터 상속받은 필드를 읽을수 있도록.!!
+        BlueThSpeaker mySpeaker =new BlueThSpeaker("JBL","검정색");
 
         System.out.println("모델:" + mySpeaker.model);
         System.out.println("색상:" + mySpeaker.color);

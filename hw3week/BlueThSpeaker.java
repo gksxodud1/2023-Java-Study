4bdf21492a2192770a8d17e2112970363011406a
gksxodud1 <133472378+gksxodud1@users.noreply.github.com>
2023-05-28 23:26:03 +0900

    Update BlueThSpeaker.java

--- hw3week/BlueThSpeaker.java
+++ hw3week/BlueThSpeaker.java
@@ -2,7 +2,7 @@ package hw3week;
 
 public class BlueThSpeaker extends Speaker{
     public BlueThSpeaker(String model, String color) {
-        super();
+        (여기에 부모의 기본 생성자를 호출할수 있는것을 쓰세요)
         this.model = model;
         this.color = color;
         System.out.println("BlueThspeaker 모델 컬러 생성자 실행 ");

c92b32f360164183ea8ed7e3e7064674f7a3767b
gksxodud1 <133472378+gksxodud1@users.noreply.github.com>
2023-05-30 00:52:24 +0900

    박준수

---
+++ hw3week/imade/importStar/ImportStar.java
@@ -0,0 +1,16 @@
+package quiz.imade.importStar;
+
+public class ImportStar {
+    String a;
+
+    String b = "*";
+    public ImportStar(String a) {
+        System.out.println("별이 내립니다.");
+        for(int i=0; i<9; i++) {
+            b += a;
+            System.out.println(b);
+        }
+    }
+
+}
+
---
+++ hw3week/imade/imstar.java
@@ -0,0 +1,9 @@
+package quiz.imade;
+import quiz.imade.importStar.ImportStar;
+
+public class imstar {
+    public static void main(String[] args) {
+        ImportStar imstar = new ImportStar("*");
+    }
+}
+

b0f020be921461c0ba3bc486b68a31c6a08f609e
gksxodud1 <133472378+gksxodud1@users.noreply.github.com>
2023-06-19 18:40:48 +0900

    개인문제 5주차

---
+++ Dog.java
@@ -0,0 +1,7 @@
+package hw5;
+
+public class Dog {
+    public void eat(Fruit fruit) {
+        fruit.eat();
+    }
+}
---
+++ Dogeat.java
@@ -0,0 +1,14 @@
+package hw5;
+
+public class Dogeat {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+
+        //매개값으로 Apple 객체를 제공하고 dog() 메소드를 호출하세요.
+
+
+        //매개값으로 Banana 객체를 제공하고 dog() 메소드를 호출하세요.
+
+
+    }
+}

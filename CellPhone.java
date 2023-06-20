c93af3b9c46434812c90be547b1a79209315e901
gksxodud1 <133472378+gksxodud1@users.noreply.github.com>
2023-06-20 21:42:15 +0900

    대현문제

---
+++ CellPhone.java
@@ -0,0 +1,13 @@
+package weekFive;
+
+public class CellPhone {
+    public Phone phone;
+
+    public void use (Phone phone) {
+        phone.use();
+}
+
+    public void turnon() {
+        phone.use();
+    }
+}

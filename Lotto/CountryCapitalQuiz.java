49739d99ee95685732382faa3d1ef13c251d11f2
gksxodud1 <133472378+gksxodud1@users.noreply.github.com>
2023-07-12 21:15:46 +0900

    Update CountryCapitalQuiz.java

--- Lotto/CountryCapitalQuiz.java
+++ Lotto/CountryCapitalQuiz.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class CountryCapitalQuiz {
     public static void main(String[] args) {
-        Map<String, String> countryCapitalMap = new HashMap<>();
+        Map<String, String> countryCapitalMap = new HashMap<>();  //문자 니까 스트링 두개 넣어주고
         countryCapitalMap.put("한국", "서울");
         countryCapitalMap.put("미국", "워싱턴 D.C.");
         countryCapitalMap.put("일본", "도쿄");
@@ -19,10 +19,10 @@ public class CountryCapitalQuiz {
         String country = scanner.nextLine();
 
         if (countryCapitalMap.containsKey(country)) {
-            String capital = countryCapitalMap.get(country);
+            String capital = countryCapitalMap.get(country);  //  capital ( 수도)이랑 나라 country  지정해준거 넣구
             System.out.println(country + "의 수도는 " + capital + "입니다.");
         } else {
-            System.out.println("해당 나라의 수도 정보를 찾을 수 없습니다.");
+            System.out.println("해당 나라의 수도 정보를 찾을 수 없습니다."); //출력
         }
 
         scanner.close();
